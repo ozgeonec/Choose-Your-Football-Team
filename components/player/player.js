32d91ba6a2f4d3c5aa61e34f2e53810597d602c3
ozgeonec <ozgenuronec@gmail.com>
2020-11-11 00:46:59 +0300

    scroll effect added to divisions

--- components/player/player.js
+++ components/player/player.js
@@ -4,6 +4,7 @@ import Avatar from "../avatar/avatar";
 import Header from "../header/header";
 import Text from "../text/text";
 import ButtonBasic from "../button/button-basic";
+import * as p from "next";
 //import cn from 'classnames'
 
 //image_url
@@ -12,14 +13,17 @@ import ButtonBasic from "../button/button-basic";
 //position.name
 //player,  position, button="PICK"
 
-function Player({image_url, position, firstname, button="PICK"}) {
+function Player({image_url, firstname, lastname, button="PICK"}) {
 
     return <div className={styles.player}>
         <div className={styles.profile}>
             <Avatar src={image_url}/>
             <div className={styles.text}>
-                <Header>{firstname}</Header>
-                <Text>jjj</Text>
+                <Header className={styles.name}>{firstname}{" "}{lastname}</Header>
+
+                {/*{position.map(pos => <Text key={pos.id}> {pos.name} </Text>)}*/}
+                    <Text>Striker</Text>
+
             </div>
         </div>
         <ButtonBasic className={styles.buttonBasic}>{button}</ButtonBasic>
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,10 @@ import fetcher from "../lib/fetch";
 import Player from "../components/player/player";
 
 function HomePage() {
-    // const { data, error } = useSWR('https://api.scoutium.com/api/clubs/4029/players?count=100', fetcher)
-    // if (error) return <div>failed to load</div>
-    // if (!data) return <div>loading...</div>
-    // //console.log(data)
-    return (
-        <Layout>
 
-            {/*{data?.players.map((player) => {*/}
-            {/*    return <Player key={player.id} {...player}/>*/}
-            {/*})}*/}
-        </Layout>
+    return (
+        <Layout/>
+
     )
 
 }
